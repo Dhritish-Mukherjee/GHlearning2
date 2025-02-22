2cbdeb62212ea08256f24fecd5731b70b58f10fc
503 Error <coderdhritish18@gmail.com>
2025-02-23 04:05:48 +0530

    first commit

---
+++ PascalTriangle.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    
+    int rows;
+    scanf("%d", &rows);
+
+
+    for (int i = 0; i < rows; i++) {
+
+       
+        for (int j = 0; j < rows - i; j++) {
+            printf("  "); 
+        }
+
+        int C = 1; 
+
+        
+        for (int k = 0; k <= i; k++) {
+            printf("%4d", C); 
+            C = C * (i - k) / (k + 1); 
+        }
+        printf("\n"); 
+    }
+    return 0;
+}
