737f7864d5f91103a9153d2f281a4e31008c1537
Dhritish-Mukherjee <coderdhritish18@gmail.com>
2025-02-25 03:37:04 +0530

    Add files via upload

---
+++ FullPyramidTriangle.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+
+    int n;
+    scanf("%d", &n);
+
+    for(int row =1; row<=n; row++){
+
+        for(int space=n; space>row; space--){
+            printf(" ");
+        }
+    
+        for(int col=0; col<2*row-1; col++){
+            printf("*");
+        }
+
+        printf("\n");
+
+    }
+
+}
